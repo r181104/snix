2566974acd45113c3b2956f0b5aaaf8664a1b1e9
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-08-02 01:39:09 +0530

    s

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -50,7 +50,7 @@
   services.xserver.enable = true;
   services.xserver.videoDrivers = [ "nvidia" ];
   services.displayManager.sddm.enable = true;
-  services.displayManager.sddm.wayland.enable = true;
+# services.displayManager.sddm.wayland.enable = true;
   services.xserver.desktopManager.budgie.enable = true;
   services.xserver.xkb = {
     layout = "us";
