f82c04352f4dfa746ade422f9c2b09ee8b8e3f40
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-08-02 08:43:28 +0530

    s

--- nixos/modules/graphics.nix
+++ nixos/modules/graphics.nix
@@ -1,9 +1,22 @@
 { config, pkgs, ... }:
 
 {
-  hardware.graphics.enable = true;
-  hardware.nvidia.open = true;
-  hardware.nvidia.modesetting.enable = true;
-  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.graphics = {
+    enable = true;
+  };
+  hardware.nvidia = {
+    open = false;
+    powerManagement.enable = true;
+    modesetting.enable = true;
+    nvidiaSettings = true;
+  };
+  hardware.nvidia.prime = {
+    offload.enable = true;
+    intelBusId = "PCI:0:2:0";
+    nvidiaBusId = "PCI:1:0:0";
+  };
+  services.xserver.videoDrivers = [ "intel" "nvidia" ];
+  services.thermald.enable = true;
+  services.tlp.enable = true;
   boot.kernelParams = [ "nomodeset" ];
 }
