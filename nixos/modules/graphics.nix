d0bf1e5335e1dfc0dbdb0985111e2c60a93d1cdf
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-08-08 04:52:40 +0530

    s

--- nixos/modules/graphics.nix
+++ nixos/modules/graphics.nix
@@ -1,13 +1,13 @@
 { config, pkgs, lib, ... }:
 
 {
-    hardware.nvidia.open = false;
     hardware.nvidia.prime = {
         intelBusId = "PCI:0@0:2:0";
         nvidiaBusId = "PCI:0@1:0:0";
         offload.enable = true;
     };
     hardware.nvidia = {
+    open = false;
     nvidiaSettings = true;
     modesetting.enable = true;
     forceFullCompositionPipeline = true;
