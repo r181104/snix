ad215effa66cdbd33107cbfb10250799871d2623
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-08-09 23:44:50 +0530

    s

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -38,7 +38,6 @@ vim.pack.add({
   { src = "https://github.com/stevearc/oil.nvim" },
   { src = "https://github.com/ibhagwan/fzf-lua" },
   { src = "https://github.com/nvim-tree/nvim-web-devicons" },
-  { src = "https://github.com/neovim/nvim-lspconfig" },
   { src = "https://github.com/mason-org/mason.nvim" },
   { src = "https://github.com/nvim-treesitter/nvim-treesitter" },
   { src = "https://github.com/chomosuke/typst-preview.nvim" },
