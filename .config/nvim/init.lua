8bc7b1ab062b4fe8649f0531b82f15b1474b1b46
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-08-16 04:43:06 +0530

    s

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -149,59 +149,6 @@ fzf.setup({
   },
 })
 
-local cmp = require('cmp')
-local luasnip = require('luasnip')
-
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body) -- For snippet support
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    }),
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' }, -- LSP-based completion
-    { name = 'luasnip' },  -- Snippets
-    { name = 'buffer' },   -- Current buffer words
-    { name = 'path' },     -- File system paths
-  })
-})
-
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
-})
-
 require("noice").setup({
   lsp = {
     override = {
@@ -305,8 +252,18 @@ require("oil").setup({
 
 vim.cmd(":hi statusline guibg=NONE")
 
-vim.lsp.enable({ "lua_ls", "svelte-language-server", "tinymist", "emmetls", "pylsp" })
-
+vim.lsp.enable({
+  "html",          -- HTML
+  "cssls",         -- CSS
+  "ts_ls",         -- JavaScript/TypeScript
+  "jdtls",         -- Java
+  "clangd",        -- C++
+  "rust_analyzer", -- Rust
+  "pyright",       -- Python
+  "sqlls",         -- SQL
+  "lua_ls",        -- Lua
+  "bashls",
+})
 vim.api.nvim_create_autocmd('LspAttach', {
   callback = function(ev)
     local opts = { buffer = ev.buf }
@@ -323,6 +280,24 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
     client.server_capabilities.completionProvider = true
   end
+})
+
+local cmp = require("cmp")
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require("luasnip").lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "path" },
+  }),
 })
 
 vim.cmd("set completeopt+=noselect")
