204d32372cf9e4b44e8774d6c33e04e6d98c6e59
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-08-16 07:02:37 +0530

    s

---
+++ .config/nvim/init.lua
@@ -0,0 +1,54 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+require( 'lazy' ).setup( 'plugins' )
+
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.termguicolors = true
+vim.o.cursorline = true
+vim.o.cursorcolumn = true
+vim.o.showmode = false
+vim.o.signcolumn = "yes"
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.softtabstop = 2
+vim.o.expandtab = true
+vim.o.smartindent = true
+vim.o.autoindent = true
+vim.o.tabstop = 4
+vim.o.wrap = true
+vim.o.linebreak = true
+vim.o.showbreak = "↪"
+vim.o.sidescroll = 1
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.updatetime = 300
+vim.o.timeout = true
+vim.o.timeoutlen = 500
+vim.o.swapfile = false
+vim.o.backup = false
+local undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.fn.mkdir(undodir, "p")
+vim.o.undodir = undodir
+vim.o.undofile = true
+vim.o.hlsearch = true
+vim.o.incsearch = true
+vim.o.scrolloff = 8
+vim.o.winborder = "rounded"
