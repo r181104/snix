1063764b071f2da784b8b188d6f620aafe2b60fa
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-08-16 20:54:43 +0530

    s

--- .config/nvim/lua/lpc/lsp.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'neovim/nvim-lspconfig',
-    config = function()
-      require 'lspconfig'.setup()
-    end
-},
-vim.lsp.config ( "lua_ls", {
-  cmd = { '$HOME/.config/nvim/lsp/lua_ls' },
-})
