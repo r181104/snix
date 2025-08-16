265a378117ac7e34a0a850abf6c51b34164aa972
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-08-17 05:00:50 +0530

    s

--- .config/nvim/lua/lpc/lsp.lua
+++ .config/nvim/lua/lpc/lsp.lua
@@ -43,7 +43,7 @@ return {
     require("mason-lspconfig").setup({
       ensure_installed = {
         -- "lua_ls",
-        "rust_analyzer",
+        -- "rust_analyzer",
         "gopls",
         "tailwindcss",
       },
