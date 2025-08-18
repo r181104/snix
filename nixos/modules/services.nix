9aeb12e0dfe1421256bedb583817843f5560d5f2
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-08-19 04:33:17 +0530

    s

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   services.xserver.enable = true;
-  services.xserver.videoDrivers = ["intel" "nvidia"];
+  services.xserver.videoDrivers = ["intel"];
   services.displayManager.sddm = {
     enable = true;
     package = pkgs.kdePackages.sddm;
