7f13e34587cf8d4e8bc6278506dbbe1c6db3b41a
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-08-19 05:34:45 +0530

    s

--- nixos/modules/hypr.nix
+++ nixos/modules/hypr.nix
@@ -3,5 +3,28 @@
   pkgs,
   ...
 }: {
+  environment.systemPackages = with pkgs; [
+    pyprland
+    hypridle
+    hyprland-qt-support
+    hyprland-qtutils
+    hyprlock
+    hyprpicker
+    xdg-desktop-portal-hyprland
+    xdg-desktop-portal-wlr
+    xdg-desktop-portal-gtk
+    xdg-desktop-portal
+    grimblast-git
+    waybar
+    swww
+    wlogout
+    wl-clipboard
+    wlroots
+    libsForQt5.xwaylandvideobridge
+    xwayland-run
+    nautilus
+    nwg-look
+  ];
   programs.hyprland.enable = true;
+  programs.xwayland.enable = true;
 }
