57d6df6f66f4627e8f932dce80b15377cd7413f0
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-08-19 07:15:01 +0530

    s

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -4,7 +4,7 @@
   ...
 }: {
   services.xserver.enable = true;
-  services.xserver.videoDrivers = ["intel" "nouveau"];
+  services.xserver.videoDrivers = ["intel"];
   services.gnome.gnome-keyring.enable = true;
   services.displayManager.sddm = {
     enable = true;
