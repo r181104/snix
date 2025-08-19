c0f5ead77b546c9187555ea5ad9a0f1752e1094c
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-08-19 20:54:40 +0530

    s

--- nixos/modules/swayfx.nix
+++ nixos/modules/swayfx.nix
@@ -4,8 +4,17 @@
   lib,
   ...
 }: {
+  programs.sway = {
+    enable = true;
+    xwayland.enable = true;
+  };
+
   environment.systemPackages = with pkgs; [
     swayfx
+    swaybg
+    swaylock-effects
+    swayidle
+    wofi
     imv
     mpd
     mpc
