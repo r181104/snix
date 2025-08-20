d196b4259044782f8471cbe6c9d514a5b82b7a9d
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-08-20 10:08:08 +0530

    s

--- .config/nvim/lua/plugins/line.lua
+++ .config/nvim/lua/plugins/line.lua
@@ -3,8 +3,8 @@ return {
 	config = function()
 		require("lualine").setup({
 			options = {
-				theme = "catppuccin",
-                style = "frappe",
+				theme = "tokyonight",
+				-- style = "",
 				section_separators = { left = "", right = "" },
 				component_separators = { left = "", right = "" },
 			},
