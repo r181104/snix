c6016df89b386e0d00bd86ba6f076fc702316346
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-08-20 09:54:17 +0530

    s

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -1,13 +1,46 @@
 return {
-	"saghen/blink.cmp",
-	dependencies = "rafamadriz/friendly-snippets",
-	version = "1.*",
-	opts = {
-		-- 'enter' for enter to accept
-		keymap = { preset = "default" },
-		appearance = { use_nvim_cmp_as_default = true, nerd_font_variant = "mono" },
-		-- completion = { documentation = { auto_show = true } },
-		fuzzy = { implementation = "prefer_rust_with_warning" },
-		signature = { enabled = true },
+	{
+		"hrsh7th/cmp-nvim-lsp",
+	},
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+		config = function() end,
+	},
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {},
+		config = function()
+			local cmp = require("cmp")
+			require("luasnip.loaders.from_vscode").lazy_load()
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-b>"] = cmp.mapping.scroll_docs(-4),
+					["<C-f>"] = cmp.mapping.scroll_docs(4),
+					["<C-y>"] = cmp.mapping.complete(),
+					["<C-e>"] = cmp.mapping.abort(),
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+				}),
+				sources = cmp.config.sources({
+					{ name = "nvim_lsp" },
+					{ name = "luasnip" },
+				}, {
+					{ name = "buffer" },
+				}),
+			})
+		end,
 	},
 }
