828ad7e7cdb0b4c5c6a8943b0ee2167a0c966352
rishabh181104 <rishabhhaldiya18@gmail.com>
2025-08-20 06:44:13 +0530

    s

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -78,6 +78,8 @@ vim.pack.add({
 	-- File tree
 	{ src = "https://github.com/stevearc/oil.nvim" },
 	{ src = "https://github.com/nvim-tree/nvim-web-devicons" },
+	-- Makes us see where something begins and where something ends as well
+	{ src = "https://github.com/lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 	-- Autopairs
 	{ src = "https://github.com/windwp/nvim-autopairs", event = "InsertEnter" },
 	-- Just so you know what's going on
@@ -287,6 +289,27 @@ require("colorizer").setup({
 		always_update = true,
 	},
 })
+
+local highlight = {
+	"RainbowRed",
+	"RainbowYellow",
+	"RainbowBlue",
+	"RainbowOrange",
+	"RainbowGreen",
+	"RainbowViolet",
+	"RainbowCyan",
+}
+local hooks = require("ibl.hooks")
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+	vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+	vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+	vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+	vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+	vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+	vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+	vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+require("ibl").setup({ indent = { highlight = highlight } })
 
 vim.diagnostic.config({
 	-- update_in_insert = true,
